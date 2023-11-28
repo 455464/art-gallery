d042c5709df6212ee1d71b3b81b75e84457272a1
quan <momiji.no.oto@gmail.com>
2023-11-28 14:15:10 +0700

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore, collection} from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCBDUHT2kmwrJOk-ZZBfFznhzLCfKXYK6k",
-  authDomain: "art-gallery-28554.firebaseapp.com",
-  projectId: "art-gallery-28554",
-  storageBucket: "art-gallery-28554.appspot.com",
-  messagingSenderId: "533797048054",
-  appId: "1:533797048054:web:e5ac9725dbb47c90f4e6d7"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
